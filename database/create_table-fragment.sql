5fefa62db9fcaf81c6f09ca762865f68d2b87af0
Danilo Ferreira e Silva <danilofes@gmail.com>
2012-12-11 08:53:32 -0200

    Criação da coluna endLine na tabela Fragment.

--- database/create_table-fragment.sql
+++ database/create_table-fragment.sql
@@ -2,5 +2,12 @@ create table dcc890.Fragment(
 	id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
 	duplicationId INTEGER,
 	line INTEGER,
+	endLine INTEGER,
 	path VARCHAR(255)
 );
+
+create index idx_path on dcc890.Fragment(path asc);
+
+update dcc890.Fragment set endLine = (select lines from dcc890.Duplication dup where dup.id = duplicationId);
+update dcc890.Fragment set endLine = (line + endLine - 1);
+commit;
